c9a07235587b874dbd3bebd912fa5f397cb65269
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-17 20:04:47 +0530

    sqlite3 updated

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -23,9 +23,25 @@ class Form(StatesGroup):
     group = State()
 
 
-def store_in_db(id):
-    conn = sqlite3.connect('users.db')
-    print("Successfully Connected")
+def get_group_from_db(chat_id):
+    query = "SELECT id, grp FROM USERS WHERE id=?"
+    data = []
+    try:
+        conn = sqlite3.connect('data.db')
+        cursor = conn.execute(query, [chat_id])
+        for row in cursor:
+            data = list(row)
+
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+        return -1
+
+    finally:
+        if conn:
+            conn.close()
+            print(data)
+            print('SQLite Connection closed')
+            return data
 
 
 @dp.message_handler(commands=['start'])
@@ -50,19 +66,13 @@ async def cancel_handler(message: types.Message, state: FSMContext):
 
     current_state = await state.get_state()
     if current_state is None:
-        # User is not in any state, ignoring
         return
-
-    # Cancel state and inform user about it
     await state.finish()
     await message.reply('Cancelled.')
 
 
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
-    """Process user name"""
-
-    # Finish our conversation
     await state.finish()
     await message.reply(f"Hello, {message.text}")  # <-- Here we get the name
 
@@ -76,7 +86,6 @@ async def ask_group(message: types.message):
 @dp.message_handler(state=Form.group)
 async def process_name(message: types.Message, state: FSMContext):
     await state.finish()
-    # <-- Here we get the name
     await message.reply(f"Your group is, {message.text}")
 
 
@@ -88,16 +97,15 @@ async def echo(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.chat.id)
+    get_group_from_db(message.chat.id)
 
 
-async def send_message(message):
-
-    await bot.send_message(705621896, message)
+async def send_message():
+    await bot.send_message(705621896, "message")
 
 
 if __name__ == '__main__':
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(send_message("Hi"))
-    # loop.close()
-    asyncio.run(send_message("hi"))
+    executor.start(dp, send_message())
     executor.start_polling(dp, skip_updates=True)
+    # executor.start_polling(dp, send_message("Hi"), skip_updates=True)
+    # asyncio.run(send_message("hi"))
