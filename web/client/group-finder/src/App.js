b02950029c9adebf30b724f0da21762c72ce4c38
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-22 23:53:26 +0530

    Update App.js

--- web/client/group-finder/src/App.js
+++ web/client/group-finder/src/App.js
@@ -1,62 +1,19 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { grey, pink } from "@mui/material/colors";
-import Avatar from "@mui/material/Avatar";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import React from "react";
+import SelectTextFields from "./components/home/selection";
+import Title from "./components/home/title";
 
-export default function BasicStack() {
+export default function App() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container
-        maxWidth="sm"
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%,-50%)",
-        }}
-      >
-        <Box
-          sx={{
-            height: "50vh",
-            border: "1px solid",
-            borderColor: grey[300],
-            backgroundColor: grey[50],
-          }}
-        >
-          <Avatar sx={{ bgcolor: grey, margin: "50px auto" }}>
-            <AccountCircleIcon />
-          </Avatar>
-          <Container sx={{ alignSelf: "center" }}>
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": {
-                  m: 1,
-                  width: "25ch",
-                },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                sx={{ alignSelf: "center" }}
-                id="outlined-basic"
-                label="Enter your ID"
-                variant="outlined"
-              />
-            </Box>
-            <Button variant="contained" sx={{ alignSelf: "center" }}>
-              Outlined
-            </Button>
-          </Container>
-        </Box>
-      </Container>
-    </React.Fragment>
+    <div
+      className="d-flex align-items-center justify-content-center flex-column"
+      style={{ height: "100vh" }}
+    >
+      <div>
+        <SelectTextFields />
+      </div>
+      <div>
+        <SelectTextFields />
+      </div>
+    </div>
   );
 }
