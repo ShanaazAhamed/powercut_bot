312cc2a0b65f9029996709696eb2367a545b488c
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-22 23:53:02 +0530

    Update index.js

--- web/client/group-finder/src/index.js
+++ web/client/group-finder/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App";
-import { StyledEngineProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <App />
   </React.StrictMode>
 );
