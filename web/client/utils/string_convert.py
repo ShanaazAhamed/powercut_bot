e73590697f522a3994a11949ffc6eb6cb58dd4b3
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-23 02:26:50 +0530

    updated

---
+++ web/client/utils/string_convert.py
@@ -0,0 +1,22 @@
+def get(text):
+    areas = text.split(",")
+    res = ""
+    for i in areas:
+        if i == "" or i == '\n':
+            continue
+        else:
+            r = str(i).strip()
+            if r != "":
+                f_r = ""
+                for j in r:
+                    if j != '\n':
+                        f_r += j
+                if f_r != "":
+                    res += f'"{str(f_r).strip()}",'
+    print(res)
+    with open("./test.txt", 'w') as f:
+        f.write(res)
+
+
+get(''' Weligopolatown,Kongasthanna,Yatipasgamuwa,Kottibulwala,Magayaya,Galpaya,Thaligama
+yaya,Karawilayaya  ''')
