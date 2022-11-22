0ae930311a7c0c170c4298f9119f7f03ce3acab0
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-22 17:31:33 +0530

    updated

--- web/client/group-finder/src/App.js
+++ web/client/group-finder/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { grey, pink } from "@mui/material/colors";
+import Avatar from "@mui/material/Avatar";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-function App() {
+export default function BasicStack() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <Container
+        maxWidth="sm"
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+        }}
+      >
+        <Box
+          sx={{
+            height: "50vh",
+            border: "1px solid",
+            borderColor: grey[300],
+            backgroundColor: grey[50],
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Avatar sx={{ bgcolor: grey, margin: "50px auto" }}>
+            <AccountCircleIcon />
+          </Avatar>
+          <Container sx={{ alignSelf: "center" }}>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": {
+                  m: 1,
+                  width: "25ch",
+                },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                sx={{ alignSelf: "center" }}
+                id="outlined-basic"
+                label="Enter your ID"
+                variant="outlined"
+              />
+            </Box>
+            <Button variant="contained" sx={{ alignSelf: "center" }}>
+              Outlined
+            </Button>
+          </Container>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
