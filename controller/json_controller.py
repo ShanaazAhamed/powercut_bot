d561da575c3e28111d93d7dd0c141947828aa87c
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-25 11:59:58 +0530

    get_groups_by_time function added

--- controller/json_controller.py
+++ controller/json_controller.py
@@ -6,8 +6,13 @@ def getTimes():
     with open("data.json", 'r') as json_obj:
         data = dict(json.load(json_obj))
         times = list(data.values())
-    # print(times)
     return times
+
+
+def getDict():
+    with open("data.json", 'r') as json_obj:
+        data = dict(json.load(json_obj))
+    return data
 
 
 def get_interrupt_times():
@@ -21,10 +26,29 @@ def get_interrupt_times():
         else:
             temp_time = times[0].split('-')
             start_time.append(temp_time[0])
-
     return list(set(start_time))
+
+
+def get_groups_by_time(time):
+    groups = []
+    dict_ = dict(getDict())
+    for grp, t in dict_.items():
+        times = t.split(",")
+        if len(times) > 1:
+            for j in times:
+                temp_time = j.split('-')
+                start_time = temp_time[0]
+                if start_time == time:
+                    groups.append(grp)
+        else:
+            temp_time = times[0].split('-')
+            start_time = temp_time[0]
+            if start_time == time:
+                groups.append(grp)
+    return groups
 
 
 if __name__ == "__main__":
     times = get_interrupt_times()
-    print(times)
+    groups = get_groups_by_time('6:00PM')
+    print(groups)
