415170ce6d840749e23a8a7da71d3d6dcb64bfa1
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-26 01:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bot import telegrambot as tel
 from controller.json_controller import get_interrupt_times
 import threading
 import multiprocessing
+from datetime import datetime
+
+available_times = []
 
 
 def web_scrap():
@@ -16,17 +19,40 @@ def web_scrap():
         pass
 
 
+def get_interrupt():
+    global available_times
+    available_times = get_interrupt_times()
+
+
+def send_message(chat_id):
+    pass
+
+
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
 
 def web_scrap_process():
+    global available_times
     schedule.every().day.at("04:30").do(run_threaded, web_scrap)
+    schedule.every().day.at("05:00").do(run_threaded, get_interrupt)
 
     while 1:
         schedule.run_pending()
-        times = get_interrupt_times()
+        if len(available_times) != 0:
+            for i in available_times:
+                now = datetime.today().strftime("%H:%M%p")
+                hour = int(now.split(":")[0])
+                if hour < 10:
+                    now = now[0:]
+                if i == now:
+                    """
+                    TODO: get the list of groups id by their id & grp
+                    TODO : iterate through the list & implement send message for each of them 
+                    TODO: <optional> Maintain thread pool for above
+                    """
+                    print(i)
 
 
 def telegram_process():
