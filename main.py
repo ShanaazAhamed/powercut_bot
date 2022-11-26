23b2ca11fc1873b8efb2e84236242d4a232e9330
Shanaaz Ahamed <77115237+ShanaazAhamed@users.noreply.github.com>
2022-11-27 01:34:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,35 @@ import schedule
 from tools import webscrap as ws
 from bot import telegrambot as tel
 from controller.json_controller import get_interrupt_times, get_groups_by_time
+from controller.time_controller import to_twenty_four, before_an_hour
 from bot.handlers.db_handler import get_list_of_id
 import threading
 import multiprocessing
 from datetime import datetime
+import time
+
+from aiogram import Bot
+from dotenv import load_dotenv
+from os import getenv
+import logging
 
 
-# available_times = []
-# def get_interrupt():
-#     global available_times
-#     available_times = get_interrupt_times()
+def send_message(chat_id):
+    load_dotenv()
+    API_TOKEN = getenv("TOKEN")
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=API_TOKEN)
+    bot.send_message(chat_id, "Message")
+
+
+available_times = []
+
+
+def get_interrupt():
+    global available_times
+    interrupt_times = get_interrupt_times()
+    available_times = interrupt_times
+    # print(available_times)
 
 
 def web_scrap():
@@ -24,48 +43,30 @@ def web_scrap():
         pass
 
 
-def send_message(chat_id):
-    print('message send to', chat_id)
-
-
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
 
 def web_scrap_process():
-    available_times = []
-
-    def get_interrupt():
-        global available_times
-        available_times = get_interrupt_times()
-        print(available_times, "********************")
+    global available_times
 
     schedule.every().day.at("04:30").do(run_threaded, web_scrap)
-    schedule.every().day.at("11:19").do(run_threaded, get_interrupt)
+    schedule.every().day.at("01:22").do(run_threaded, get_interrupt)
 
     while 1:
         schedule.run_pending()
-        print(available_times)
+        now = datetime.today().strftime("%H:%M")
         if len(available_times) != 0:
             for i in available_times:
-                now = datetime.today().strftime("%H:%M%p")
-                hour = int(now.split(":")[0])
-                if hour < 10:
-                    now = now[0:]
-                if i == now:
-                    print("RUNNING")
-                    """
-                    TODO: get the list of groups id by time
-                        1. get the groups by time <list>
-                        2.get_list_of_id
-                    TODO : iterate through the list & implement send message for each of them 
-                    TODO: <optional> Maintain thread pool for above
-                    """
+                temp = before_an_hour(i)
+                i_std = to_twenty_four(temp)
+                if i_std == now:
                     groups = get_groups_by_time(i)
                     chat_ids = get_list_of_id(groups)
                     for i in chat_ids:
-                        send_message(i)
+                        send_message(int(i))
+                    time.sleep(60)
 
 
 def telegram_process():
@@ -73,6 +74,7 @@ def telegram_process():
 
 
 if __name__ == "__main__":
+    # tel.bot.send_message(705621896, "Hello")
     p1 = multiprocessing.Process(target=web_scrap_process)
     p2 = multiprocessing.Process(target=telegram_process)
     p1.start()
